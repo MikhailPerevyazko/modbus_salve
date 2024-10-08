73369eef9b156d726a77145580503251a2b4635b
Mikhail Perevyazko <perevyazko.misha2001@gmail.com>
2024-10-08 15:09:43 +0300

    some change

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,26 @@ mod config;
 mod transport_tcp;
 
 use rmodbus::{client::ModbusRequest, ModbusProto};
-use std::io::{Read, Write};
+use std::{
+    io::{Read, Write},
+    net::TcpStream,
+};
 use transport_tcp::TransportCommand;
 
 fn main() {
-    modbus();
-}
-
-fn modbus() {
     // Подключение по TCP
     let mut stream = config::transport_tcp().connect();
     // Создаем объект запроса
     let mut mreq = ModbusRequest::new(1, ModbusProto::TcpUdp);
+
+    modbus(&mut stream, &mut mreq);
+}
+
+fn modbus(stream: &mut TcpStream, mreq: &mut ModbusRequest) {
     // Создаем вектор запроса
     let mut request: Vec<u8> = Vec::new();
 
-    // Установим значения койлов
+    // Установим значения для coil
     mreq.generate_set_coils_bulk(
         0,
         &[true, true, true, true, true, true, true, true, true, true],
