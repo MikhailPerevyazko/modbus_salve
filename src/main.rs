613e0ca9576c5703a9fe42a18e6cadf5678097c7
Mikhail Perevyazko <perevyazko.misha2001@gmail.com>
2024-10-09 09:46:41 +0300

    Запись койлов и запрос на получение их состояния

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod cmd;
 mod config;
 mod transport_tcp;
 
-use rmodbus::{client::ModbusRequest, ModbusProto};
+use rmodbus::{client::ModbusRequest, guess_response_frame_len, ModbusProto};
 use std::{
     io::{Read, Write},
     net::TcpStream,
@@ -14,35 +14,67 @@ fn main() {
     let mut stream = config::transport_tcp().connect();
     // Создаем объект запроса
     let mut mreq = ModbusRequest::new(1, ModbusProto::TcpUdp);
-
-    modbus(&mut stream, &mut mreq);
+    get_coils_status(&mut stream, &mut mreq);
 }
 
-fn modbus(stream: &mut TcpStream, mreq: &mut ModbusRequest) {
+pub fn get_coils_status(stream: &mut TcpStream, mreq: &mut ModbusRequest) {
     // Создаем вектор запроса
     let mut request: Vec<u8> = Vec::new();
-
     // Установим значения для coil
     mreq.generate_set_coils_bulk(
         0,
-        &[true, true, true, true, true, true, true, true, true, true],
+        &[false, true, true, true, true, true, true, true, true, false],
         &mut request,
     )
     .unwrap();
 
-    // Записываем запрос в stream
-    stream.write(&request).unwrap();
-    println!("Request: {:?}", request);
+    println!("Запрос на запись койлов: {:?}", request);
 
-    // Считываем 12 байтов ответа в буфер
-    let mut buf = [0u8; 12];
+    // Записываем запрос в потко
+    stream.write_all(&request).unwrap();
+
+    // Получаем заголовок ответа из потока
+    let mut buf = [0u8; 6];
     stream.read_exact(&mut buf).unwrap();
 
     let mut response = Vec::new();
     response.extend_from_slice(&buf);
-    println!("Response: {:?}", response);
 
-    // Проверим ошибки внутри модбаса
-    mreq.parse_ok(&request).unwrap();
+    // Читаем хвост ответа
+    let head = guess_response_frame_len(&buf, ModbusProto::TcpUdp).unwrap();
+    if head > 6 {
+        let mut tail = vec![0u8; (head - 6) as usize];
+        stream.read_exact(&mut tail).unwrap();
+        response.extend(tail);
+        println!("Ответ на запись койлов: {:?}\n", response);
+    }
+
+    // Проверяем на наличие ошибок modbus coils
     mreq.parse_ok(&response).unwrap();
+
+    // Получаем состояние койлов
+    mreq.generate_get_coils(0, 10, &mut request).unwrap();
+    stream.write_all(&request).unwrap();
+    println!("Запрос на запись состояния койлов: {:?}", request);
+
+    let mut buf = [0u8; 6];
+    stream.read_exact(&mut buf).unwrap();
+
+    let mut response = Vec::new();
+    response.extend_from_slice(&buf);
+    let head = guess_response_frame_len(&buf, ModbusProto::TcpUdp).unwrap();
+    if head > 6 {
+        let mut tail = vec![0u8; (head - 6) as usize];
+        stream.read_exact(&mut tail).unwrap();
+        response.extend(tail);
+    }
+    println!("Ответ на состояние койлов: {:?}\n", response);
+
+    let mut data = Vec::new();
+
+    // Парсим состояние койлов
+    mreq.parse_bool(&response, &mut data).unwrap();
+    for (i, c) in data.iter().enumerate() {
+        println!("Coil #{} - {}", i, c);
+    }
 }
--- src/transport_tcp.rs
+++ src/transport_tcp.rs
@@ -1,36 +1,24 @@
-// use rmodbus::ModbusProto;
 use std::net::TcpStream;
 
 pub trait TransportCommand {
-    // fn modbus_proto(&self) -> ModbusProto;
     fn connect(&mut self) -> TcpStream;
-    // fn disconnect(&mut self);
 }
 
 #[derive(Debug)]
 pub struct TransportTCP {
     pub host_port: String,
     pub connected: bool,
-    // pub stream: Option<TcpStream>,
 }
 
 impl TransportCommand for TransportTCP {
-    // fn modbus_proto(&self) -> ModbusProto {
-    //     ModbusProto::TcpUdp
-    // }
-
     fn connect(&mut self) -> TcpStream {
         if self.connected {
             todo!()
         } else {
             // Устанавливаем соединение
             let stream = TcpStream::connect(&self.host_port).unwrap();
-            println!("Соединение с устройством {} установлено\n", self.host_port);
+            println!("Соединение с устройством {} установлено.\n", self.host_port);
             stream
         }
     }
-    // fn disconnect(&mut self) {
-    //     self.connected = false;
-    //     println!("Соединение с устройством {} отключено", self.host_port);
-    // }
 }
