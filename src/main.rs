4e24a1275d3caa5a7293c57d7158238294b1661d
Mikhail Perevyazko <perevyazko.misha2001@gmail.com>
2024-10-17 09:52:51 +0300

    add some fix

--- src/main.rs
+++ src/main.rs
@@ -8,31 +8,31 @@ use registers_map::Coils;
 use rmodbus::{client::ModbusRequest, ModbusProto};
 use transport_tcp::TransportCommand;
 
+pub fn parse_type_storage(map: Coils) -> String {
+    let type_storage = map.type_storage;
+    type_storage
+}
+
 fn main() {
-    //? Получаем карту регистров по имени "Temp"
+    //? Получаем карту регистров по имени "Temp" и парсим type storage
     let map_coils = registers_map::call_to_reg_map("Voltage".to_string());
+    let type_store = parse_type_storage(map_coils.clone());
 
     //? Подключение по TCP и создание объекта запроса
     let mut stream = config::transport_tcp().connect();
     let mut mreq = ModbusRequest::new(map_coils.unit_id, ModbusProto::TcpUdp);
 
-    let type_storage = parse_type_storage(map_coils.clone());
-
     //? Команды Modbus
-    if type_storage == "DO" {
+    if type_store == "DO" {
         modbus::set_coils(&mut stream, &mut mreq, map_coils.start_address);
         modbus::parse_status_coils(&mut stream, &mut mreq, map_coils.start_address);
-    } else if map_coils.type_storage == "DI" {
-        println!("nothing");
-    } else if map_coils.type_storage == "AI" {
-        println!("nothing");
-    } else if map_coils.type_storage == "AO" {
+    } else if type_store == "DI" {
+        todo!()
+    } else if type_store == "AI" {
+        todo!()
+    } else if type_store == "AO" {
+        todo!()
     } else {
-        println!("This coil's name doesn't exists!")
+        println!("This type storage is wrong!")
     }
-}
-
-pub fn parse_type_storage(map_coils: Coils) -> String {
-    let type_storage = map_coils.type_storage;
-    type_storage
 }
