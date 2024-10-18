a1954012d9a5b1997fbe341cff0a01bfeb2c77d6
Mikhail Perevyazko <perevyazko.misha2001@gmail.com>
2024-10-18 10:38:11 +0300

    add some fix

--- src/main.rs
+++ src/main.rs
@@ -22,19 +22,22 @@ pub fn parse_parameters_type(map: Coils) -> String {
 }
 
 pub fn modbus_commands() {
-    //? Получаем карту регистров по имени "Temp" и парсим type storage
-    let map_coils = registers_map::call_to_reg_map("Voltage".to_string());
-    let type_store = parse_type_storage(map_coils.clone());
-    let para_type = parse_parameters_type(map_coils.clone());
+    // Получаем карту регистров по имени "Temp" и парсим type storage
+    let find_param_name = String::from("Voltage");
 
-    //? Подключение по TCP и создание объекта запроса
+    let map_coils = registers_map::call_to_reg_map(find_param_name);
+
+    let type_store = parse_type_storage(map_coils.clone());
+    let param_type = parse_parameters_type(map_coils.clone());
+
+    // Подключение по TCP и создание объекта запроса
     let mut stream = config::transport_tcp().connect();
     let mut mreq = ModbusRequest::new(map_coils.unit_id, ModbusProto::TcpUdp);
 
-    //? Команды Modbus в зависиомости от type storage
+    // Команды Modbus в зависиомости от type storage
     if type_store == "DO" {
         modbus::set_coils(&mut stream, &mut mreq, map_coils.start_address);
-        modbus::parse_status_coils(&mut stream, &mut mreq, map_coils.start_address, para_type);
+        modbus::parse_status_coils(&mut stream, &mut mreq, map_coils.start_address, param_type);
     } else if type_store == "DI" {
         todo!()
     } else if type_store == "AI" {
