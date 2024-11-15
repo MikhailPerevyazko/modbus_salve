fb6abf5e9ec926ac6bad43265d8d2253df264dd4
Mikhail Perevyazko <perevyazko.misha2001@gmail.com>
2024-11-15 16:29:56 +0300

    add config.rs new file

---
+++ src/config.rs
@@ -0,0 +1,90 @@
+use serde::{Deserialize, Serialize};
+use std::fs;
+
+// Структура для TCP соединения
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct TcpConnection {
+    pub host: String,
+    pub port: String,
+}
+// Структура для UDP соединения
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct UdpConnection {
+    pub host: String,
+    pub port: String,
+    pub listen_host: String,
+    pub listen_port: String,
+}
+// Структура для RTU соединения
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct RtuConnection {
+    pub name: String,
+    pub baud_rate: i32,
+    pub data_bits: i32,
+    pub flow_control: Option<String>,
+    pub parity: Option<String>,
+    pub stop_bits: i32,
+}
+
+// #[derive(Debug, Serialize, Deserialize, Clone)]
+// // #[serde(tag = "protocol_type", rename_all = "lowercase")]
+// pub enum Connection {
+//     Tcp(TcpConnection),
+//     Udp(UdpConnection),
+//     Rtu(RtuConnection),
+// }
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Connection {
+    pub host: Option<String>,
+    pub port: Option<String>,
+    pub listen_host: Option<String>,
+    pub listen_port: Option<String>,
+    pub name: Option<String>,
+    pub baud_rate: Option<i32>,
+    pub data_bits: Option<i32>,
+    pub flow_control: Option<String>,
+    pub parity: Option<String>,
+    pub stop_bits: Option<i32>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ClientModbusConfig {
+    pub protocol_type: String,
+    // #[serde(flatten)]
+    pub connection: Connection,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct FunctionParameter {
+    pub name: String,
+    pub unit_id: i32,
+    pub ptype: String,
+    pub start_address: i32,
+    pub mstorage: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ClientModbusConfigs {
+    pub connection_configs: Vec<ClientModbusConfig>,
+    pub parameters: Vec<FunctionParameter>,
+}
+
+pub fn load_configuration(path_to_config: &str) -> Vec<ClientModbusConfig> {
+    let configuration = fs::read_to_string(path_to_config.to_string()).unwrap();
+    let configs: ClientModbusConfigs = serde_yaml::from_str(&configuration.as_str()).unwrap();
+    let vec_client_modbus_config = configs.connection_configs;
+    vec_client_modbus_config
+}
+
+pub fn parse_to_json_format() -> Vec<String> {
+    let configs =
+        load_configuration("/home/Mikhail/projects/try_to_modbus/modbus_tcp/modbus_config.yaml");
+
+    let mut vec_configs: Vec<String> = Vec::new();
+    for config in configs {
+        let parsed_config = serde_json::to_string(&config).unwrap();
+        vec_configs.push(parsed_config.clone());
+    }
+    vec_configs
+}
--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,13 @@
-use serde::{Deserialize, Serialize};
-use std::fs;
+mod config;
+use config::parse_to_json_format;
 
 fn main() {
-    let x = parse_to_json_format();
-    println!("{:#?}", x);
+    test();
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct TcpConnection {
-    pub host: String,
-    pub port: String,
-}
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct UdpConnection {
-    pub host: String,
-    pub port: String,
-    pub listen_host: String,
-    pub listen_port: String,
-}
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct RtuConnection {
-    pub name: String,
-    pub baud_rate: i32,
-    pub data_bits: i32,
-    pub flow_control: Option<String>,
-    pub parity: Option<String>,
-    pub stop_bits: i32,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub enum Connection {
-    TcpConnection,
-    UdpConnection,
-    RtuConnection,
-}
-
-// #[derive(Debug, Serialize, Deserialize, Clone)]
-// pub struct Connection {
-//     pub host: Option<String>,
-//     pub port: Option<String>,
-//     pub listen_host: Option<String>,
-//     pub listen_port: Option<String>,
-//     pub name: Option<String>,
-//     pub baud_rate: Option<i32>,
-//     pub data_bits: Option<i32>,
-//     pub flow_control: Option<String>,
-//     pub parity: Option<String>,
-//     pub stop_bits: Option<i32>,
-// }
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct ClientModbusConfig {
-    pub protocol_type: String,
-    pub connection: Connection,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct FunctionParameter {
-    pub name: String,
-    pub unit_id: i32,
-    pub ptype: String,
-    pub start_address: i32,
-    pub mstorage: String,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct ClientModbusConfigs {
-    pub connection_configs: Vec<ClientModbusConfig>,
-    pub parameters: Vec<FunctionParameter>,
-}
-
-pub fn load_configuration(path_to_config: &str) -> Vec<ClientModbusConfig> {
-    let configuration = fs::read_to_string(path_to_config.to_string()).unwrap();
-    let configs: ClientModbusConfigs = serde_yaml::from_str(&configuration.as_str()).unwrap();
-    let vec_client_modbus_config = configs.connection_configs;
-    vec_client_modbus_config
-}
-
-pub fn parse_to_json_format() -> Vec<String> {
-    let configs =
-        load_configuration("/home/Mikhail/projects/try_to_modbus/modbus_tcp/modbus_config.yaml");
-
-    let mut vec_configs: Vec<String> = Vec::new();
-    for config in configs {
-        let parsed_config = serde_json::to_string(&config).unwrap();
-        vec_configs.push(parsed_config.clone());
+pub fn test() {
+    let json_vec = parse_to_json_format();
+    for i in json_vec {
+        println!("{:#?}\n", i);
     }
-    vec_configs
 }
