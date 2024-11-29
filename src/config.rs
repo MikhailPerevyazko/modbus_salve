893325475e9c67c9d02c474ea23af77eafa1039d
Mikhail Perevyazko <perevyazko.misha2001@gmail.com>
2024-11-29 11:45:11 +0300

    add some

--- src/config.rs
+++ src/config.rs
@@ -5,20 +5,21 @@ use std::fs;
 pub struct Connection {
     pub host: Option<String>,
     pub port: Option<String>,
-    // pub listen_host: Option<String>,
-    // pub listen_port: Option<String>,
-    // pub name: Option<String>,
-    // pub baud_rate: Option<i32>,
-    // pub data_bits: Option<i32>,
-    // pub flow_control: Option<String>,
-    // pub parity: Option<String>,
-    // pub stop_bits: Option<i32>,
+    pub listen_host: Option<String>,
+    pub listen_port: Option<String>,
+    pub name: Option<String>,
+    pub baud_rate: Option<i32>,
+    pub data_bits: Option<i32>,
+    pub flow_control: Option<String>,
+    pub parity: Option<String>,
+    pub stop_bits: Option<i32>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct ClientModbusConfig {
+pub struct ConnectionModbusConfig {
     pub protocol_type: String,
     pub connection: Connection,
+    pub channel_id: i32,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -32,170 +33,166 @@ pub struct FunctionParameter {
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct ClientModbusConfigs {
-    pub connection_configs: Vec<ClientModbusConfig>,
+    pub connection_configs: Vec<ConnectionModbusConfig>,
     pub parameters: Vec<FunctionParameter>,
 }
 
-pub fn load_connections_config(path_to_config: &str) -> Vec<ClientModbusConfig> {
+pub fn load_connections_config(path_to_config: &str) -> Vec<ConnectionModbusConfig> {
     let configuration = fs::read_to_string(path_to_config.to_string()).unwrap();
     let configs: ClientModbusConfigs = serde_yaml::from_str(&configuration.as_str()).unwrap();
     let vec_client_modbus_config = configs.connection_configs;
-
     return vec_client_modbus_config;
 }
 
-pub fn json_connection_config() -> Vec<String> {
+pub fn json_connection_config() -> Vec<ConnectionModbusConfig> {
     let path = String::from("/home/Mikhail/projects/try_to_modbus/modbus_tcp/modbus_config.yaml");
     let configs = load_connections_config(&path);
-
-    let mut vec_configs: Vec<String> = Vec::new();
-    for config in configs {
-        let parsed_config = serde_json::to_string(&config).unwrap();
-        vec_configs.push(parsed_config.clone());
-    }
-
-    return vec_configs;
+    return configs;
 }
 
-pub fn load_configs(path_to_config: &str) -> Vec<FunctionParameter> {
+pub fn load_param_configs(path_to_config: &str) -> Vec<FunctionParameter> {
     let configuration = fs::read_to_string(path_to_config.to_string()).unwrap();
     let configs: ClientModbusConfigs = serde_yaml::from_str(&configuration.as_str()).unwrap();
     let parameters = configs.parameters;
-
     return parameters;
 }
 
-pub fn get_parameters_from_config() -> Vec<Vec<String>> {
-    // Записываем вектор из структур с yaml
-    let content =
-        load_configs("/home/Mikhail/projects/try_to_modbus/modbus_tcp/modbus_config.yaml");
+pub fn sort_fn_params() -> Vec<Vec<FunctionParameter>> {
+    let vec_fn_params =
+        load_param_configs("/home/Mikhail/projects/try_to_modbus/modbus_tcp/modbus_config.yaml");
 
-    let mut do_vec: Vec<FunctionParameter> = Vec::new();
-    let mut di_vec: Vec<FunctionParameter> = Vec::new();
-    let mut ao_vec: Vec<FunctionParameter> = Vec::new();
-    let mut ai_vec: Vec<FunctionParameter> = Vec::new();
+    let mut vec_do_fn_params: Vec<FunctionParameter> = Vec::new();
+    let mut vec_di_fn_params: Vec<FunctionParameter> = Vec::new();
+    let mut vec_ao_fn_params: Vec<FunctionParameter> = Vec::new();
+    let mut vec_ai_fn_params: Vec<FunctionParameter> = Vec::new();
 
-    // Разделить струкрутры по четырем векторам в зависимости от mstorage
-    for v in content {
-        if v.mstorage == "DO" {
-            do_vec.push(v);
-        } else if v.mstorage == "DI" {
-            di_vec.push(v);
-        } else if v.mstorage == "AO" {
-            ao_vec.push(v);
-        } else if v.mstorage == "AI" {
-            ai_vec.push(v);
+    for fn_param in vec_fn_params {
+        if fn_param.mstorage == "DO" {
+            vec_do_fn_params.push(fn_param);
+        } else if fn_param.mstorage == "DI" {
+            vec_di_fn_params.push(fn_param);
+        } else if fn_param.mstorage == "AO" {
+            vec_ao_fn_params.push(fn_param);
+        } else if fn_param.mstorage == "AI" {
+            vec_ai_fn_params.push(fn_param);
         } else {
-            println!("Неверный mstorage!")
+            println!("Error mstorage in config file.")
+        }
+    }
+    vec_do_fn_params.sort_by(|a, b| a.start_address.cmp(&b.start_address));
+    vec_di_fn_params.sort_by(|a, b| a.start_address.cmp(&b.start_address));
+    vec_ao_fn_params.sort_by(|a, b| a.start_address.cmp(&b.start_address));
+    vec_ai_fn_params.sort_by(|a, b| a.start_address.cmp(&b.start_address));
+
+    let mut sorted_vec_fn_params: Vec<Vec<FunctionParameter>> = Vec::new();
+    sorted_vec_fn_params.push(vec_do_fn_params);
+    sorted_vec_fn_params.push(vec_di_fn_params);
+    sorted_vec_fn_params.push(vec_ao_fn_params);
+    sorted_vec_fn_params.push(vec_ai_fn_params);
+
+    return sorted_vec_fn_params;
+}
+
+pub fn change_start_addr_count(params_store: &Vec<FunctionParameter>) -> Vec<(i32, i32)> {
+    let mut start_addrs = Vec::new();
+    for fn_param in params_store {
+        start_addrs.push(fn_param.start_address);
+    }
+    let mut addrs_vec = Vec::new();
+    for fn_param in params_store {
+        addrs_vec.push(fn_param.start_address);
+    }
+    if addrs_vec.is_empty() {
+        println!("Вектор пуст.");
+    }
+
+    let mut count = 1;
+    let mut first_element = addrs_vec[0];
+    let mut result = Vec::new();
+
+    for i in 1..addrs_vec.len() {
+        if addrs_vec[i] == addrs_vec[i - 1] + 1 {
+            count += 1;
+        } else {
+            result.push((first_element, count));
+            count = 1;
+            first_element = addrs_vec[i];
         }
     }
 
-    // Сортировать векторы от min к max по start_address
-    do_vec.sort_by(|a, b| a.start_address.cmp(&b.start_address));
-    di_vec.sort_by(|a, b| a.start_address.cmp(&b.start_address));
-    ao_vec.sort_by(|a, b| a.start_address.cmp(&b.start_address));
-    ai_vec.sort_by(|a, b| a.start_address.cmp(&b.start_address));
+    result.push((first_element, count));
 
-    // Парсим векторы в json-формат
-    let mut json_vec_do: Vec<String> = Vec::new();
-    for params in &do_vec {
-        let parsed_params = serde_json::to_string(&params).unwrap();
-        json_vec_do.push(parsed_params.clone());
-    }
+    return result;
+}
 
-    let mut json_vec_di: Vec<String> = Vec::new();
-    for params in &di_vec {
-        let parsed_params = serde_json::to_string(&params).unwrap();
-        json_vec_di.push(parsed_params.clone());
-    }
+pub fn get_all_tasks() -> Vec<Vec<String>> {
+    let params_store = sort_fn_params();
+    // println!("{:?}", params_store);
+    let do_params_store = params_store.get(0).unwrap();
+    let di_params_store = params_store.get(1).unwrap();
+    let ao_params_store = params_store.get(2).unwrap();
+    let ai_params_store = params_store.get(3).unwrap();
 
-    let mut json_vec_ao: Vec<String> = Vec::new();
-    for params in &ao_vec {
-        let parsed_params = serde_json::to_string(&params).unwrap();
-        json_vec_ao.push(parsed_params.clone());
-    }
+    let new_do_param = change_start_addr_count(do_params_store);
+    let new_di_param = change_start_addr_count(di_params_store);
+    let new_ao_param = change_start_addr_count(ao_params_store);
+    let new_ai_param = change_start_addr_count(ai_params_store);
 
-    let mut json_vec_ai: Vec<String> = Vec::new();
-    for params in &ai_vec {
-        let parsed_params = serde_json::to_string(&params).unwrap();
-        json_vec_ai.push(parsed_params.clone());
-    }
+    let mut id = 1;
 
-    //? ID функции
-    let mut id: i32 = 1;
-    //?Count константы
-    const COUNT_BOOL: i32 = 1;
-    const COUNT_FLOAT: i32 = 2;
-
-    let mut vec_fn_do: Vec<String> = Vec::new();
-    for parameters in do_vec {
-        let count = match &parameters.ptype.to_lowercase()[..] {
-            "bool" => COUNT_BOOL,
-            "float" => COUNT_FLOAT,
-            _ => 1,
-        };
-
+    let do_unit_id = do_params_store[0].unit_id;
+    let mut do_tasks_vec: Vec<String> = Vec::new();
+    for params in &new_do_param {
+        let (start_addr, count) = params;
         id += 1;
-        let read_coil_status: String = format!(
+        let read_coil_status = format!(
             r#"{{"id":{},"unit":{},"operation":"ReadCoilStatus","address":{},"count":{}}}"#,
-            id, parameters.unit_id, parameters.start_address, count
+            id, do_unit_id, start_addr, count
         );
-        vec_fn_do.push(read_coil_status);
+        do_tasks_vec.push(read_coil_status);
     }
 
-    let mut vec_fn_di: Vec<String> = Vec::new();
-    for parameters in di_vec {
-        let count = match &parameters.ptype.to_lowercase()[..] {
-            "bool" => COUNT_BOOL,
-            "float" => COUNT_FLOAT,
-            _ => 1,
-        };
-
+    let di_unit_id = di_params_store[0].unit_id;
+    let mut di_tasks_vec: Vec<String> = Vec::new();
+    for params in &new_di_param {
+        let (start_addr, count) = params;
         id += 1;
         let read_input_status = format!(
             r#"{{"id":{},"unit":{},"operation":"ReadInputStatus","address":{},"count":{}}}"#,
-            id, parameters.unit_id, parameters.start_address, count
+            id, di_unit_id, start_addr, count
         );
-        vec_fn_di.push(read_input_status);
+        di_tasks_vec.push(read_input_status);
     }
 
-    let mut vec_fn_ao: Vec<String> = Vec::new();
-    for parameters in ao_vec {
-        let count = match &parameters.ptype.to_lowercase()[..] {
-            "bool" => COUNT_BOOL,
-            "float" => COUNT_FLOAT,
-            _ => 1,
-        };
-
+    let ao_unit_id = ao_params_store[0].unit_id;
+    let mut ao_tasks_vec: Vec<String> = Vec::new();
+    for params in &new_ao_param {
+        let (start_addr, count) = params;
         id += 1;
-        let read_holding_registers: String = format!(
+        let read_holding_registers = format!(
             r#"{{"id":{},"unit":{},"operation":"ReadHoldingRegisters","address":{},"count":{}}}"#,
-            id, parameters.unit_id, parameters.start_address, count
+            id, ao_unit_id, start_addr, count
         );
-        vec_fn_ao.push(read_holding_registers);
+        ao_tasks_vec.push(read_holding_registers);
     }
 
-    let mut vec_fn_ai: Vec<String> = Vec::new();
-    for parameters in ai_vec {
-        let count = match &parameters.ptype.to_lowercase()[..] {
-            "bool" => COUNT_BOOL,
-            "float" => COUNT_FLOAT,
-            _ => 1,
-        };
-
+    let ai_unit_id = ai_params_store[0].unit_id;
+    let mut ai_tasks_vec: Vec<String> = Vec::new();
+    for params in &new_ai_param {
+        let (start_addr, count) = params;
         id += 1;
-        let read_holding_registers: String = format!(
+        let read_input_registers = format!(
             r#"{{"id":{},"unit":{},"operation":"ReadInputRegisters","address":{},"count":{}}}"#,
-            id, parameters.unit_id, parameters.start_address, count
+            id, ai_unit_id, start_addr, count
         );
-        vec_fn_ai.push(read_holding_registers);
+        ai_tasks_vec.push(read_input_registers);
     }
 
-    let mut vec_func_tasks = Vec::new();
-    vec_func_tasks.push(vec_fn_do);
-    vec_func_tasks.push(vec_fn_di);
-    vec_func_tasks.push(vec_fn_ao);
-    vec_func_tasks.push(vec_fn_ai);
+    let mut all_tasks_json = Vec::new();
+    all_tasks_json.push(do_tasks_vec);
+    all_tasks_json.push(di_tasks_vec);
+    all_tasks_json.push(ao_tasks_vec);
+    all_tasks_json.push(ai_tasks_vec);
 
-    return vec_func_tasks;
+    return all_tasks_json;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,53 @@
 mod config;
 mod connect;
 
-use config::{get_parameters_from_config, json_connection_config};
+use config::get_all_tasks;
+use config::json_connection_config;
 use connect::conneting;
 use rmodbus_client::ModBusClient;
 use std::{thread::sleep, time::Duration};
 
 fn main() {
-    // Получить вектор настроек для соединения
-    let connection_config = json_connection_config();
-    let client_tcp = ModBusClient::new();
-    // Подключение к modbus slave
-    conneting(&client_tcp, connection_config);
-    // Получить вектор задач
-    let vec_tasks = get_parameters_from_config();
+    call_modbus();
+}
 
-    // Основной цикл
+pub fn call_modbus() {
+    //? Получить вектор настроек для соединения
+    let connection_config = json_connection_config();
+    let mut vec_configs_tcp: Vec<String> = Vec::new();
+    let mut vec_configs_udp: Vec<String> = Vec::new();
+    let mut vec_configs_rtu: Vec<String> = Vec::new();
+
+    let client_tcp = ModBusClient::new();
+    let client_udp = ModBusClient::new();
+    let client_rtu = ModBusClient::new();
+
+    for config in connection_config {
+        if config.channel_id == 1 {
+            let parsed_config = serde_json::to_string(&config).unwrap();
+            vec_configs_tcp.push(parsed_config.clone());
+            println!("channel id = 1");
+        } else if config.channel_id == 2 {
+            let parsed_config = serde_json::to_string(&config).unwrap();
+            vec_configs_udp.push(parsed_config.clone());
+            println!("channel id = 2");
+        } else if config.channel_id == 3 {
+            let parsed_config = serde_json::to_string(&config).unwrap();
+            vec_configs_rtu.push(parsed_config.clone());
+            println!("channel id = 3")
+        } else {
+            println!("unknown channel id")
+        }
+    }
+    conneting(&client_tcp, vec_configs_tcp);
+    conneting(&client_udp, vec_configs_udp);
+    conneting(&client_rtu, vec_configs_rtu);
+
+    // Получить вектор задач
+    let vec_tasks = get_all_tasks();
+
+    //? Основной цикл *//
     loop {
-        println!("{:#?}", vec_tasks);
         for vec in &vec_tasks {
             println!("\n");
             for task in vec {
@@ -28,10 +58,10 @@ fn main() {
                 while !client_tcp.have_got_responses() {
                     sleep(Duration::from_millis(1));
                 }
-                let answer = client_tcp.last_response_str();
-                println!("Ответ: {:?}", answer.unwrap());
+                let answer = client_tcp.last_response_str().unwrap();
+                println!("Ответ: {:#?}", answer);
             }
-            sleep(Duration::from_millis(500));
+            sleep(Duration::from_millis(200));
         }
         break;
     }
